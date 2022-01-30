e4d1dd22668afefe7240b4ebfb182a01c9b53b16
TatianaYo <yoienko@yandex.ru>
2022-01-30 15:50:19 +0300

    Added program code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿void TransferArrayElem(string[] arrOfStr)
+{
+    int strSize = 3;
+    int sizeNewArr = 0;
+    for (int i = 0; i < arrOfStr.Length; i++)
+    {
+        if (arrOfStr[i].Length <= strSize) sizeNewArr++;
+    }
+    if(sizeNewArr > 0)
+    {
+        string[] newArrOfStr = new string[sizeNewArr];
+        int index = 0;
+        for(int i = 0; i < arrOfStr.Length; i++)
+        {
+            if (arrOfStr[i].Length <= strSize)
+            {
+                newArrOfStr[index] = arrOfStr[i];
+                index++;
+            }
+        }
+        Console.WriteLine("New array: [{0}]", string.Join(", ", newArrOfStr));
+    }
+    else Console.WriteLine("New array: []");
+}
+
+string[] arr = { "7dklb", "6789", "4e5d", "hgj", "yj", "5ghjck", "5hfls", "67h", "7sgdj", "fghjl" };
+TransferArrayElem(arr);
